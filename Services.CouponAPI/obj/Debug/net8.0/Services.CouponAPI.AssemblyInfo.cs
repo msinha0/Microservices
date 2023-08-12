05c862f40a1b8bb637229101e59208bae33e467c
msinha0 <dev.msinha0@gmail.com>
2023-08-12 22:10:20 +0530

    Removed JUNK2

--- Services.CouponAPI/obj/Debug/net8.0/Services.CouponAPI.AssemblyInfo.cs
+++ Services.CouponAPI/obj/Debug/net8.0/Services.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bd83c7ce4ae25083be6257a34771fda651a9618")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c94bf3b133dfb6a0f01ce381b03aba503b417281")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
