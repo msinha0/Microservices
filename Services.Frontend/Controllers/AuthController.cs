b9cbfc70aab94ecf005e7924173e0f44cd722eb6
msinha0 <dev.msinha0@gmail.com>
2023-08-15 11:24:38 +0530

    Added auth controller in the frontend proj

---
+++ Services.Frontend/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Frontend.Models;
+using Services.Frontend.Service.IService;
+
+namespace Services.Frontend.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new LoginRequestDto();
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
